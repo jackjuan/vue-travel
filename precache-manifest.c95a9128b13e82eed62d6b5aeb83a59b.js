4491e3efa2b75b8844bfbdfc66ecad48265576c6
JUAN1996 <1352787908@qq.com>
2019-01-25 12:10:44 +0800

    travel

---
+++ precache-manifest.c95a9128b13e82eed62d6b5aeb83a59b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9f0efe973aadca77b718",
+    "url": "/js/chunk-vendors.f024b2c4.js"
+  },
+  {
+    "revision": "b7d251c6055340e7ab3e",
+    "url": "/js/app.7c9d8968.js"
+  },
+  {
+    "revision": "12a1193a3f63859276f302334f161844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d14d1ce9422c75bc3f3bbfe0935b88b",
+    "url": "/img/iconfont.7d14d1ce.svg"
+  },
+  {
+    "revision": "ee758fdde1de7e7fc1004b6aa917df0d",
+    "url": "/img/detail.ee758fdd.png"
+  },
+  {
+    "revision": "9f0efe973aadca77b718",
+    "url": "/css/chunk-vendors.b32848a0.css"
+  },
+  {
+    "revision": "b7d251c6055340e7ab3e",
+    "url": "/css/app.083aa4bb.css"
+  }
+];
